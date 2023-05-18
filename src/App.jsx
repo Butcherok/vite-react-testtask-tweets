c92b78197b656cfe638ddca3c3e2acfdf4b7e7ad
Butcherok <butcherok231@gmail.com>
2023-05-18 10:02:17 +0300

    fix: title template

--- src/App.jsx
+++ src/App.jsx
@@ -17,7 +17,7 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h1>Template</h1>
+      <h2>Template</h2>
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>
           count is {count}
