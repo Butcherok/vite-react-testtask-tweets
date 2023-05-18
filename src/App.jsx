30df81ac8b6e3b318989fcf8ec7f0779a3301ea8
Butcherok <butcherok231@gmail.com>
2023-05-18 12:24:59 +0300

    remove: clean code

--- src/App.jsx
+++ src/App.jsx
@@ -17,10 +17,6 @@ function App() {
         </a>
       </div>
       <h1>Vite + React</h1>
-      <h2>Template</h2>
-      <h2>Template</h2>
-      <h2>Template</h2>
-      <h2>Template</h2>
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>
           count is {count}
@@ -30,9 +26,7 @@ function App() {
         </p>
       </div>
       <p className="read-the-docs">
-        Click on the Vite and React logos to learn more Click on the Vite and
-        React logos to learn more Click on the Vite and React logos to learn
-        more Click on the Vite and React logos to learn more
+        Click on the Vite and React logos to learn more
       </p>
     </>
   );
