20c3cd3e4feba4c5f298f9ec229829595c661bce
Butcherok <butcherok231@gmail.com>
2023-05-18 12:05:25 +0300

    ADD: more titles

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,10 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import reactLogo from './assets/react.svg';
+import viteLogo from '/vite.svg';
+import './App.css';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
@@ -18,6 +18,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <h2>Template</h2>
+      <h2>Template</h2> <h2>Template</h2> <h2>Template</h2>
       <div className="card">
         <button onClick={() => setCount(count => count + 1)}>
           count is {count}
@@ -33,4 +34,4 @@ function App() {
   );
 }
 
-export default App
+export default App;
