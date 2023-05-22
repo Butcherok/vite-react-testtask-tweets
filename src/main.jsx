15ea4f283a735ff2cdfba39c9717632db432b94e
Butcherok <butcherok231@gmail.com>
2023-05-22 08:27:04 +0300

    fix: 404

--- src/main.jsx
+++ src/main.jsx
@@ -9,13 +9,13 @@ import { theme } from './shared/constStyles';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <HashRouter>
-        <BrowserRouter
-          basename={import.meta.env.DEV ? '/' : '/vite-react-testtask-tweets/'}
-        >
+      <BrowserRouter
+        basename={import.meta.env.DEV ? '/' : '/vite-react-testtask-tweets/'}
+      >
+        <HashRouter>
           <App />
-        </BrowserRouter>
-      </HashRouter>
+        </HashRouter>
+      </BrowserRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
