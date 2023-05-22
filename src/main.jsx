1f37ab601c301ce783af5ff23b442dac7196ba9f
Butcherok <butcherok231@gmail.com>
2023-05-22 08:23:58 +0300

    fix: 404

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { BrowserRouter, HashRouter } from 'react-router-dom';
 import './index.css';
 import App from './components/App.jsx';
 import { ThemeProvider } from '@mui/material';
@@ -9,9 +9,13 @@ import { theme } from './shared/constStyles';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <ThemeProvider theme={theme}>
-      <BrowserRouter basename={import.meta.env.DEV ? '/' : '/vite-react-testtask-tweets/'}>
-        <App />
-      </BrowserRouter>
+      <HashRouter>
+        <BrowserRouter
+          basename={import.meta.env.DEV ? '/' : '/vite-react-testtask-tweets/'}
+        >
+          <App />
+        </BrowserRouter>
+      </HashRouter>
     </ThemeProvider>
   </React.StrictMode>
 );
